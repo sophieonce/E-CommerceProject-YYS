5bf0439a97a4f40d5d7481932a7bc042d245fdd3
sophie <sophie071019@gmail.com>
2017-04-11 15:40:34 -0500

    Add products to cart and update cart.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,43 @@
 class CartsController < ApplicationController
+
   def index
-  end
+      @cart = cart
+    end
+
+    def add
+      id = params[:id]
+      cart[id] ? cart[id] = cart[id] + 1 : cart[id] = 1
+      flash[:notice] = "Added Successfully!"
+      redirect_to :back
+    end
+
+# update cart quantity
+    def update
+      id = params[:id]
+      if params[:commit] == '-'
+        if(cart[id] > 1)
+        cart[id] = cart[id] - 1
+        else
+        cart[id] = 1
+       end
+      redirect_to :back
+      elsif params[:commit] == '+'
+        cart[id] ? cart[id] = cart[id] + 1 : cart[id] = 1
+        redirect_to :back
+      end
+    end
+
+# remove product from shopping cart
+    def destroy
+    id = params[:id]
+    session[:cart].delete(id)
+    redirect_to action: :index
+    end
+
+
+# empty shopping cart
+    def clearCart
+      session[:cart] = nil
+      redirect_to action: :index
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,17 @@ Rails.application.routes.draw do
   # CATEGORIES
   get 'categories/:id' => 'categories#show', as: 'category', id: /\d+/
 
+  # CARTS: add item, showing and clear cart
+  get 'carts' => 'carts#index'
+  get 'carts/clear' => 'carts#clearCart', as: 'cart_clear'
+  get 'carts/:id' => 'carts#add', as: 'cart', id: /\d+/
+
+  # UPDATE CARTS QUANTITY
+  patch 'carts/:id'     => 'carts#update'
+
+  # DELETE CART ITEM
+  delete 'carts/:id'  =>'carts#destroy',as: 'destroy_item', id: /\d+/
+
 
   root to: 'products#index'
 

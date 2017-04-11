825a44cedaae87d46e9a024399fff7bf7f370464
sophie <sophie071019@gmail.com>
2017-04-11 15:45:52 -0500

    Add province model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411193242) do
+ActiveRecord::Schema.define(version: 20170411204436) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -59,6 +59,14 @@ ActiveRecord::Schema.define(version: 20170411193242) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "pst"
+    t.decimal  "hst"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

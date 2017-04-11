6d6024d89a0c10ad78afeba8c42f2b8b566a758b
sophie <sophie071019@gmail.com>
2017-04-11 15:13:34 -0500

    Add header and category navigation.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  # PRODUCTS
+  get 'products'     => 'products#index'
+  get 'products/:id' => 'products#show', as: 'product', id: /\d+/
+
+  # CATEGORIES
+  get 'categories/:id' => 'categories#show', as: 'category', id: /\d+/
+
+
+  root to: 'products#index'
+
+
 end

b2dd104a3d0a09faacf1224ff364e8dd6ffa5162
sophie <sophie071019@gmail.com>
2017-04-11 15:21:01 -0500

    View products via category.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
   def show
+    @category = Category.find(params[:id])
+    @products = Product.where(:category_id => @category.id).order(:name).page(params[:page]).per(5)
   end
 end

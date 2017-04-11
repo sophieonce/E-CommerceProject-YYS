c8543e1a5bcd6bca5c47ff7453c88b5f2dac5870
sophie <sophie071019@gmail.com>
2017-04-11 15:29:57 -0500

    Search through all products or search product by category.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.order(:name).page(params[:page]).per(10)
+    if params[:search]
+    @products = @products.search(params)
+    else
+    @products = Product.order(:name).page(params[:page]).per(10)
+    end
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
   validates :name,:price,:stock, presence: true
-  
-
   mount_uploader :image, ImageUploader
+
+def self.search(params)
+   products = Product.where("name LIKE ? or description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+   products = products.where("category_id = ?", params[:category_id]) if params[:category_id].present?
+   products
+ end
+
 end
